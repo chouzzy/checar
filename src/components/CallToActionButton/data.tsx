41be64f68449bc82274714dffa9de66305c3e105
chouzzy <mathfernando@hotmail.com>
2024-07-17 17:37:14 -0300

    tel

--- src/components/CallToActionButton/data.tsx
+++ src/components/CallToActionButton/data.tsx
@@ -1,6 +1,6 @@
 
 
 export const CallToActionButtonData = {
-    href: "https://wa.me/556133457474?text=Olá, tudo bem? Acessei o site da Checar Soluções Automotivas e gostaria de um orçamento!",
+    href: "tel:+556133457474",
     instagram:"https://www.instagram.com/checarsolucoes/"
 }
