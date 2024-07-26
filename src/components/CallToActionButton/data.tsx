505f82d0e5b55bc7fc02ee0479e132267bf327d2
chouzzy <mathfernando@hotmail.com>
2024-07-26 14:23:45 -0300

    novo numero

--- src/components/CallToActionButton/data.tsx
+++ src/components/CallToActionButton/data.tsx
@@ -1,6 +1,6 @@
 
 
 export const CallToActionButtonData = {
-    href: "https://wa.me/5561999876832?text=Olá, tudo bem? Acessei o site da Checar Soluções e gostaria de falar com um atendente.",
+    href: "https://wa.me/5561998706832?text=Olá, tudo bem? Acessei o site da Checar Soluções e gostaria de falar com um atendente.",
     instagram:"https://www.instagram.com/checarsolucoes/"
 }
